cd0d36020dd70931e80e0c334201086970dc4820
Marco <mgenova03@gmail.com>
2021-01-05 22:21:44 -0500

    invoice total

--- client/src/components/Invoices/_Invoices.js
+++ client/src/components/Invoices/_Invoices.js
@@ -1,65 +1,53 @@
-import React, {useState, useCallback, useRef } from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
-import MaterialTable from 'material-table';
+import MaterialTable, { MTableToolbar } from 'material-table';
 import { GET_INVOICES } from './invoices.query'
 
 const Invoices = () => {
   
   const {data: invoicesQuery, loading: invoicesQueryLoading} = useQuery(GET_INVOICES)
   
-  const [total, setTotal] = useState(0);
-  const tableRef = React.useRef();
-  // const add = useCallback((rows) => {
-  //   const newTotal = rows.reduce((a, b) => +a + +b.saleTotal, 0)
-  //     window.setTimeout(() => {
-  //       setTotal(newTotal);
-  //     }, 5000)
+  const [components] = useState({
+    Toolbar: props => (
+      <div >
+        {props.selectedRows.length > 0  &&  
+          <div
+          style={{
+            backgroundColor: 'rgb(255, 226, 236)', 
+            fontSize: '20px', 
+            color: '#f50057',
+            height: '70px'
+          }}>
+            <p className="text-center">
+              {props.selectedRows.length} Item(s) Selected <br/>
+              Total: $ {props.selectedRows.reduce((a, b) => +a + +b.saleTotal, 0)}
+            </p>
+          </div>
+        }
 
-  // }, [])
+        <MTableToolbar {...props} />
+      </div>
+    ),
+  });
 
-  // const add = (rows) => {
-  //   setTotal(rows.reduce((a, b) => +a + +b.saleTotal, 0))
-  //   // console.log("value set is ", total);
-  // };
-
-  // useEffect(() => {
-  //   console.log("value set is ", total);
-  //    // take action when isVisible Changed
-  // }, [total])
 
   if (invoicesQueryLoading) return 'Loading...'
 
-// setTotal(prev => ({...prev, total:'11111'}));
-// setTotal((values) => ({...values, total: result}))
-// setTotal(rows.reduce((a, b) => +a + +b.saleTotal, 0))
-// tableRef.current.dataManager.changeRowSelected(false, [idx])
-  // console.log(total)
   return (
     <div>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
         /> 
-        {total}        
         <MaterialTable
-          tableRef={tableRef}
-          title="Invoices"
-          localization={{ 
-            toolbar: {
-              nRowsSelected: `{0} Selected Total: ${total}`
-            },
-          }}
-          // onSelectionChange={(rows) =>  console.log(tableRef.current)}          
+          components={components}
+          title="Invoices"    
           options={{
             paging: false,
             selection: true,
+            showTextRowsSelected: false,
           }}
-          actions={[
-            {
-              icon: 'delete',
-              onClick: (evt, data) => alert('You want to delete ' + data.length + ' rows')
-            }
-          ]}       
+      
           columns={[
             { title: 'ID', field: 'id', editable: 'never' },
             { title: 'Time Placed', field: 'createdAt', 
